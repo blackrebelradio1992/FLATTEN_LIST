56d1708030de4d170c6379eb389a4577e12509ce
Frank_Gary <blackrebelradio1992@gmail.com>
2023-10-18 09:32:20 -0500

    initial

--- flatten_list.py
+++ flatten_list.py
@@ -1,2 +1,10 @@
 def flatten_list(lst):
-    pass
+    input = lst
+    output = [] #[1, 2, 3, 4, 5, 6, 7]
+    for i in input:
+
+        output.append(input[i]) 
+        print(output)
+
+lst = [1, [2], [3, [4, 5, [6]]], 7]
+print(flatten_list(lst))
